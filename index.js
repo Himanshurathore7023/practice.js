100fae759632cf54a5166af94ddea385c178ebf4
Himanshurathore7023 <rathore.himanshu96@gmail.com>
2023-11-28 19:35:38 +0530

    intial value

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const str1 ="himanshu";
 const str2= "my name is kajal";
-const str3= "ilovemyindia"
-console.log(str1);
+const str3= "ilovemyindia";
+
+
+/*charAt
+
+console.log(str1.charAt(3));
+console.log(str2[3]);
+*/
+
+//EndWith
+console.log(str2.endsWith("kajal"));
