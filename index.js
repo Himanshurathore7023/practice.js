af5748c1f978dba99bc703c12afac3eada37900d
Himanshurathore7023 <rathore.himanshu96@gmail.com>
2023-11-29 12:03:04 +0530

    intial value

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const str1 ="himanshu";
 const str2= "my name is kajal";
 const str3= "ilovemyindia";
-
+const str4="  once upon a time   ";
 
 /*charAt
 
@@ -9,5 +9,27 @@ console.log(str1.charAt(3));
 console.log(str2[3]);
 */
 
-//EndWith
+/*EndWith
 console.log(str2.endsWith("kajal"));
+
+
+
+// trip method 
+//this method delete extra space for the line
+
+
+
+console.log(str4.trim());
+
+
+
+
+// replace and include
+
+const url="http://himanshu.com/himansshu?%20rathore"
+console.log(url.replace("%20","-"));  
+console.log(url.includes("himanshu"));
+
+*/
+const word= str4.split(" ");
+console.log(word[3]);
